ddbfb790f90ccebec9e9c8d71846ba681f6c5c33
Tim B <tbatty7@gmail.com>
2018-12-26 22:57:00 -0500

    initial commit for working resource server

---
+++ src/main/java/com/example/resourceserver/Controller.java
@@ -0,0 +1,33 @@
+package com.example.resourceserver;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class Controller {
+
+    private String message = "Hello world!";
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Map<String, String> home() {
+        return Collections.singletonMap("message", message);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public void updateMessage(@RequestBody String message) {
+        this.message = message;
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public Map<String, String> user(Principal user) {
+
+        return Collections.singletonMap("message", "user is: " + user.toString());
+    }
+
+}
---
+++ src/main/java/com/example/resourceserver/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
