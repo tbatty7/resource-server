991716d88d1ecb16c255a700e9b347afc83a915e
Tim B <tbatty7@gmail.com>
2019-01-04 22:33:55 -0500

    added authz functionality

--- src/main/java/com/example/resourceserver/Controller.java
+++ src/main/java/com/example/resourceserver/Controller.java
@@ -1,5 +1,6 @@
 package com.example.resourceserver;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,16 +15,19 @@ public class Controller {
 
     private String message = "Hello world!";
 
+    @PreAuthorize("hasRole('ROLE_RS_READ')")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Map<String, String> home() {
         return Collections.singletonMap("message", message);
     }
 
+    @PreAuthorize("hasRole('ROLE_RS_WRITE')")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public void updateMessage(@RequestBody String message) {
         this.message = message;
     }
 
+    @PreAuthorize("#oauth2.hasScope('resource-server-read')")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public Map<String, String> user(Principal user) {
 
--- src/main/java/com/example/resourceserver/DemoApplication.java
+++ src/main/java/com/example/resourceserver/DemoApplication.java
@@ -2,10 +2,12 @@ package com.example.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class DemoApplication {
 
 	public static void main(String[] args) {
